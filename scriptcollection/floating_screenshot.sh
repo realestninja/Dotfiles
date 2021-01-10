4f19834b9e8127150666a6f1542c2f3ae86d8cb3
Nikhil Nawgiri <the@realest.ninja>
2021-01-10 15:46:59 +0100

    [scriptcollection] Add i3 instructions to floating_screenshot script

--- scriptcollection/floating_screenshot.sh
+++ scriptcollection/floating_screenshot.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# following 2 lines are required in i3 config:
+# [class="^custom-float-no-border$"] floating enable
+# for_window [class="^custom-float-no-border$"] border pixel 0
+
 rect=$(xrectsel)
 sleep 0.1
 read -r w h x y <<< "$(echo "$rect" | grep -Eo '[[:digit:]]+' | tr '\n' ' ')"
