9b3c65411570fdfcac7181b73cf1fd42d5b06990
Nikhil Nawgiri <the@realest.ninja>
2023-01-05 14:50:50 +0100

    [polybar] Display only spotify in mpris string

--- polybar/modules/player-mpris-simple.sh
+++ polybar/modules/player-mpris-simple.sh
@@ -4,9 +4,9 @@
 player_status=$(playerctl -p spotify status 2> /dev/null)
 
 if [ "$player_status" = "Playing" ]; then
-    echo "▶ $(playerctl metadata artist) - $(playerctl metadata title)"
+    echo "▶ $(playerctl -p spotify metadata --format '{{ artist }} - {{ title }}')"
 elif [ "$player_status" = "Paused" ]; then
-    echo "⏸ $(playerctl metadata artist) - $(playerctl metadata title)"
+    echo "⏸ $(playerctl -p spotify metadata --format '{{ artist }} - {{ title }}')"
 else
     echo ""
 fi
